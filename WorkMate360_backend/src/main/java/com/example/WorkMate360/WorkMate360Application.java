61106f1d046532b2ac6d9f16078317c9506d6055
medhani premathilaka <mpremathilaka1@gmail.com>
2025-05-13 10:51:21 +0530

    firts commit

---
+++ WorkMate360_backend/src/main/java/com/example/WorkMate360/WorkMate360Application.java
@@ -0,0 +1,13 @@
+package com.example.WorkMate360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkMate360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkMate360Application.class, args);
+	}
+
+}
