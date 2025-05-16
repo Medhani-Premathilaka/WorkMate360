6f9b56ad163da0354579a25b28fc673260d4b92f
medhani premathilaka <mpremathilaka1@gmail.com>
2025-05-16 21:57:51 +0530

    feat: add routing with react-router-dom and implement Nav and Sidebar components

--- WorkMate360_frontend/my-app/src/App.tsx
+++ WorkMate360_frontend/my-app/src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './App.css'
+import { Home } from './pages/Home'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Login } from './pages/Login';
 function App() {
   return (
-    <div className="text-[50px]">hello</div>
+  <Router>
+    <div className='App'></div>
+    <Routes>
+      <Route path="/home" element={<Home/>}> </Route>
+      <Route path="/" element={<Login/>}> </Route>
+    </Routes>
+  </Router>
   )
 }
 
