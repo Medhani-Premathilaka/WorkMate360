51bead049659f1b72e027c0c2b864c21a6b474f0
medhani premathilaka <mpremathilaka1@gmail.com>
2025-05-16 19:14:54 +0530

    feat: implement login functionality with Login model, controller, DAO, and service

---
+++ WorkMate360_backend/src/main/java/com/example/WorkMate360/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.WorkMate360.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/login")
+public class LoginController {
+
+}
