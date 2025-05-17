851579db341492749d42ac2ee94b3da6c3befbf5
medhani premathilaka <mpremathilaka1@gmail.com>
2025-05-17 16:33:05 +0530

    feat: update CORS configuration, add Card component for profile display, and include axios dependency

--- WorkMate360_backend/src/main/java/com/example/WorkMate360/controllers/LoginController.java
+++ WorkMate360_backend/src/main/java/com/example/WorkMate360/controllers/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/login")
 public class LoginController {
 
--- WorkMate360_frontend/my-app/src/pages/Home.tsx
+++ WorkMate360_frontend/my-app/src/pages/Home.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 import './Home.css'
 import {Nav} from '@/components/Nav'
+import {Card} from '@/components/Card'
 
 export function Home() {
   return (
     <div>
       <Nav/>
-      
+      <div className='absolute top-64 left-64'>
+        
+        <Card/>
+      </div>
     </div>
   )
 }
