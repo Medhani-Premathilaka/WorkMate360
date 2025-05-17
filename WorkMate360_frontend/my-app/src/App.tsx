76f469fb85bfd7c2c35a2edafb5a4b262141cf14
medhani premathilaka <mpremathilaka1@gmail.com>
2025-05-17 22:50:45 +0530

    feat: refactor Profile model, enhance Card component with profile image and navigation, add Search component, and implement Details page with file upload functionality

--- WorkMate360_frontend/my-app/src/App.tsx
+++ WorkMate360_frontend/my-app/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Home } from './pages/Home'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Login } from './pages/Login';
+import { Details } from './pages/Details';
 function App() {
   return (
   <Router>
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/home" element={<Home/>}> </Route>
       <Route path="/" element={<Login/>}> </Route>
+      <Route path="/details" element={<Details/>}> </Route>
     </Routes>
   </Router>
   )
