76b76b2aca3907fb96435fa7e429b8f018586260
medhani premathilaka <mpremathilaka1@gmail.com>
2025-06-03 09:37:09 +0530

    Fix typo in UserCredintials component for retrieving jwtToken from localStorage

--- WorkMate360_frontend/my-app/src/pages/UserCredintials.tsx
+++ WorkMate360_frontend/my-app/src/pages/UserCredintials.tsx
@@ -39,7 +39,7 @@ export default function UserCredintials() {
     if (passwordRegex.test(formData.password1)) {
       if (formData.password1 === formData.password2) {
         const username = localStorage.getItem("username");
-        inlocalStorage.getItem("jwtToken");
+        localStorage.getItem("jwtToken");
 
         await axios.post("http://localhost:8080/changepassword", {
           username: username, // You need to send username
