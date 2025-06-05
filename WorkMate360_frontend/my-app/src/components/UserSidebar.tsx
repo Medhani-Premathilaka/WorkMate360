e7fc5f76ee0b9d51237f840fd0995688093c4ad4
medhani premathilaka <mpremathilaka1@gmail.com>
2025-06-05 14:55:18 +0530

    Refactor UserSidebar component for improved code clarity and organization; enhance logout functionality by ensuring all relevant localStorage items are cleared.

--- WorkMate360_frontend/my-app/src/components/UserSidebar.tsx
+++ WorkMate360_frontend/my-app/src/components/UserSidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { Dialog } from "@mui/material";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
