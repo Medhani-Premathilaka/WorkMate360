9bfa25b271f27c8f5ae09ad03f97c6833c1be210
medhani premathilaka <mpremathilaka1@gmail.com>
2025-06-08 00:45:09 +0530

    Update API base URL to use ngrok for dynamic routing

--- WorkMate360_frontend/my-app/src/api.ts
+++ WorkMate360_frontend/my-app/src/api.ts
@@ -2,7 +2,7 @@ import type { AxiosInstance } from "axios";
 import axios from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL:  'https://56.228.42.123:8080',
+  baseURL:  'https://1497-56-228-42-123.ngrok-free.app',
   withCredentials: true
   
 });
