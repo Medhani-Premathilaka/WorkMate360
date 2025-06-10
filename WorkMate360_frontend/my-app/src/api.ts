18a8cb7a53d76377ae1a7ab599c331519c3a82f6
medhani premathilaka <mpremathilaka1@gmail.com>
2025-06-10 17:55:07 +0530

    Update API base URL, add Vercel configuration, and modify .gitignore

--- WorkMate360_frontend/my-app/src/api.ts
+++ WorkMate360_frontend/my-app/src/api.ts
@@ -2,7 +2,7 @@ import type { AxiosInstance } from "axios";
 import axios from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "https://95c9-56-228-42-123.ngrok-free.app",
+  baseURL: "https://02c4-56-228-42-123.ngrok-free.app",
   withCredentials: false,
   headers: {
     "Accept": "application/json",
