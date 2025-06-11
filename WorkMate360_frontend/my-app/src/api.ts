93c349d957afe057b7af76491df2e1b5ded8ee87
medhani premathilaka <mpremathilaka1@gmail.com>
2025-06-11 20:50:48 +0530

    Update API base URL to the correct ngrok endpoint

--- WorkMate360_frontend/my-app/src/api.ts
+++ WorkMate360_frontend/my-app/src/api.ts
@@ -2,7 +2,7 @@ import type { AxiosInstance } from "axios";
 import axios from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "https://0157-56-228-42-123.ngrok-free.app",
+  baseURL: "https://a466-56-228-42-123.ngrok-free.app/",
   withCredentials: false,
   headers: {
     "Accept": "application/json",
