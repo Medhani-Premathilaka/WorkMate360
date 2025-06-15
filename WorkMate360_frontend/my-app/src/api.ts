b8bb048af5b2ea259aa7fb460de4b002eacf0aab
medhani premathilaka <mpremathilaka1@gmail.com>
2025-06-15 22:50:24 +0530

    Add test comment to axios import in api.ts

--- WorkMate360_frontend/my-app/src/api.ts
+++ WorkMate360_frontend/my-app/src/api.ts
@@ -1,5 +1,5 @@
 import type { AxiosInstance } from "axios";
-import axios from "axios";
+import axios from "axios"; // test
 
 const api: AxiosInstance = axios.create({
   baseURL: "https://api.workmate360.live/",
